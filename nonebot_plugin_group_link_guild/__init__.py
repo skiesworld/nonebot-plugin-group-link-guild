87051436146cb2d37b99148dac3a870ff3c25cce
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-06-19 00:53:36 +0800

    Update __init__.py
    添加插件信息

--- nonebot_plugin_group_link_guild/__init__.py
+++ nonebot_plugin_group_link_guild/__init__.py
@@ -1,15 +1,30 @@
 from typing import Union
 
 from nonebot import on_message, on_command
+from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Message
 from nonebot.params import CommandArg
 from nonebot_plugin_guild_patch import GuildMessageEvent
+
+from .config import Config
 
 from .utils import (
     msg_rule_cmd,
     msg_rule_no_cmd,
     get_message,
     send_msgs
+)
+
+__plugin_meta__ = PluginMetadata(
+    name="群频互通",
+    description="将QQ群与QQ频道的消息互通",
+    usage="在群聊发送消息即可同步",
+    config=Config,
+    type="application",
+    homepage="https://github.com/17TheWord/nonebot-plugin-group-link-guild",
+    supported_adapters=[
+        "nonebot.adapters.onebot.v11"
+    ]
 )
 
 nonebot_plugin_group_link_guild = on_message(rule=msg_rule_no_cmd, priority=31)
